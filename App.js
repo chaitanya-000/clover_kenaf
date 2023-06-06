28c598f4541c3adadc1abdf5fb93279451951c92
Chaitanya Joshi <Chaitanya99j@gmail.com>
2023-06-06 16:28:08 +0530

    chore(package.json | package-lock.json | yarn.lock | app.js): installs styled-components library
    This commit installs and tests styled-components library. There were conflicting issues during
    installing the library. Used the commandnpm install styled-components@5 --force

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import styled from 'styled-components';
 
 const App = () => {
+  const InformativeFooterText = styled.Text`
+    color: rgba(38, 174, 96, 1);
+    font-weight: 700;
+    align-self: center;
+    font-size: 215px;
+  `;
   return (
     <View>
-      <Text>Clover app</Text>
+      <InformativeFooterText>
+        This is the build to see if styled components library is working in
+        build or not
+      </InformativeFooterText>
     </View>
   );
 };
