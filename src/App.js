9f35fbb02c66992f5188de412896707d6d2844bd
Chaitanya Joshi <Chaitanya99j@gmail.com>
2023-06-06 19:08:13 +0530

    chore(package.json | package-lock.json): installs and tests react-native-async-storage/async-storage
    This commit installs the react-native-async-storage/async-storage library. Also, the library is
    tested by creating a build and running on android emulator as each library, the -force flag needs to
    be added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {InformativeFooterText} from './styledComponents';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import {width, height} from './styledComponents';
 
 const App = () => {
+  const [enteredData, setEnteredData] = useState('');
+  const storeData = async () => {
+    try {
+      await AsyncStorage.setItem('value', JSON.stringify(enteredData));
+      alert('data  stored successfully');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const readData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('value');
+      alert(value);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
-    <View>
-      <InformativeFooterText>
-        This is to test whether the imports from the styledComponents file in
-        the root directory is working or not
-      </InformativeFooterText>
+    <View style={styles.container}>
+      <Text onPress={storeData}>Store data</Text>
+      <TextInput
+        style={styles.container_input}
+        onChangeText={value => setEnteredData(value)}
+        clearButtonMode="always"
+      />
+      <Text onPress={readData}>Read data</Text>
+      <Text onPress={() => setEnteredData('')}>reset</Text>
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    // backgroundColor: 'red',
+  },
+  container_input: {
+    width: '40%',
+    height: '10%',
+    borderWidth: 3,
+  },
+});
