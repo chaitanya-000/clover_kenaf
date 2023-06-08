a436b08152edba4ff5489f729969281c97db42c9
Chaitanya Joshi <Chaitanya99j@gmail.com>
2023-06-08 18:32:05 +0530

    feat(tabs-naviagtion): adds and tests Bottoms tab navigation with a local build
    This commit installs and adds the bottoms tab naviagtion with sample screen of payment and
    dashboard. They are tested with a local build on flex 2 emulator

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import {StyleSheet, View, Text} from 'react-native';
-import React from 'react';
+import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import PaymentLogs from './screens/PaymentLogs';
-import QuickSale from './screens/QuickSale';
-import Refund from './screens/Refund';
-import BankDetails from './screens/BankDetails';
-import ProfileUpdate from './screens/ProfileUpdate';
-import Payment from './screens/Payment';
+import TabsNavigation from './TabsNavigation';
 
-const App = () => {
-  const Stack = createNativeStackNavigator();
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Payment" component={Payment} />
-        <Stack.Screen name="QuickSale" component={QuickSale} />
-        <Stack.Screen name="PaymentLogs" component={PaymentLogs} />
-        <Stack.Screen name="Refund" component={Refund} />
-        <Stack.Screen name="BankDetails" component={BankDetails} />
-        <Stack.Screen name="ProfileUpdate" component={ProfileUpdate} />
-      </Stack.Navigator>
+      <TabsNavigation />
     </NavigationContainer>
   );
-};
-
-export default App;
+}
